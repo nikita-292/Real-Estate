eaa9987ee6dfc2c61aa3c08a2e21be0fb6f3b07d
nikita-292 <145786206+nikita-292@users.noreply.github.com>
2025-05-19 19:44:14 +0530

    create sugnup

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -11,8 +12,9 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err);
 });
 
-const app = express();
 
+const app = express();
+app.use(express.json()); // middleware to parse json data from the request body
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
@@ -20,3 +22,4 @@ app.listen(3000, () => {
 
 
 app.use('/api/user', userRouter); // use the user router for the api/user route
+app.use('/api/auth', authRouter);
