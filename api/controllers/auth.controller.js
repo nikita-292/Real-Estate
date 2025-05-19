abede64e92c8aa44e55f747fdb621d8a9e3ef3de
nikita-292 <145786206+nikita-292@users.noreply.github.com>
2025-05-19 23:37:32 +0530

    create signapi route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,35 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-
-export const signup = async (req, res,next) => {
+import jwt from "jsonwebtoken";
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10); //hashSync se await asynxc use nhi krna padhtha
-  const newUser = new User({username,email,password:hashedPassword});
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json('User created successfully!');
+    res.status(201).json("User created successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+    //creating a token for future use for user so yha hum id us ekr rhe  an dsaving as it a cookies
+    //httpOnly true maltba no ther paty can have access ti vcookie and expires maltba whi kabtak rahega
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const {password :pass, ...rest} = validUser._doc ; //separated the  password 
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
