2979c10af94a124f3f7fb6f2c2a1354f2fc03bdf
nikita-292 <145786206+nikita-292@users.noreply.github.com>
2025-05-20 18:29:58 +0530

    redux

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
-import  userReducer  from './user/userSlice';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+//combine reducer mthod
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = { //local storage name 
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { user : userReducer},
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-  getDefaultMiddleware({
-    serializableCheck: false,
-  }),
-})
- 
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+export const persistor = persistStore(store);
