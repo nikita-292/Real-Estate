6852f6a2c6da7d4e7804a4138847350906af5d96
nikita-292 <145786206+nikita-292@users.noreply.github.com>
2025-05-26 16:23:14 +0530

    delete userfunctionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,6 +6,9 @@ import {
   updateUserStart,
   updateUserFailure,
   updateUserSuccess,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
 } from "../redux/user/userSlice";
 import { set } from "mongoose";
 
@@ -125,6 +128,27 @@ export default function Profile() {
     }
   };
 
+  const handleDeleteUser = async () => {
+  try {
+    dispatch(deleteUserStart());
+
+    const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+      method: 'DELETE',
+    });
+
+    const data = await res.json();
+
+    if (data.success === false) {
+      dispatch(deleteUserFailure(data.message));
+      return;
+    }
+
+    dispatch(deleteUserSuccess(data));
+  } catch (error) {
+    dispatch(deleteUserFailure(error.message));
+  }
+};
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -200,12 +224,12 @@ export default function Profile() {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
 
-          {/* <p className="text-red-700 mt-5">{error ? error :""}</p>
-        <p className="text-green-700 mt-5">{updateSuccess ?"User updated successfully!":""}</p> */}
+           {/* <p className="text-red-700 mt-5">{error ? error :""}</p> */}
+        <p className="text-green-700 mt-5">{updateSuccess ?"User updated successfully!":""}</p> 
     
     </div>
   );
