7f9234bff9d1fe301d9aae3629fb215af7a65812
nikita-292 <145786206+nikita-292@users.noreply.github.com>
2025-05-26 17:16:25 +0530

    create liating

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';  
 dotenv.config();
 
@@ -28,6 +29,7 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter); // use the user router for the api/user route
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter); // use the listing router for the api/listing route
 
 //middleware error from ip  next call for the next middleware
 // Error-handling middleware
