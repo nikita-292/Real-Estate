7e35c4ddfc7b3d5121d68a38c4e5e33ac257a30f
nikita-292 <145786206+nikita-292@users.noreply.github.com>
2025-05-30 10:14:17 +0530

    create search api routeb

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -64,13 +64,53 @@ export const getListing = async (req, res, next) => {
 
 export const getListings = async (req, res, next) => {
   try {
-    const listing = await Listing.findById(req.params.id);
+    // filter if there is a query then limit lga sakte 
+    // for offer or furnished  === search inside the database  for both true or false 
+   const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
 
-    if (!listing) {
-      return next(errorHandler(404, 'Listing not found!'));
+    if (offer === undefined || offer === 'false') {
+      offer = { $in: [false, true] };
     }
 
-    res.status(200).json(listing); 
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === 'false') {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+
+    if (parking === undefined || parking === 'false') {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === 'all') {
+      type = { $in: ['sale', 'rent'] };
+    }
+
+    const searchTerm = req.query.searchTerm || ''; //query di tohh ya blank
+
+    const sort = req.query.sort || 'createdAt'; //pahle sort kro query kro or kuch na diya toh created time ke according krdo
+
+    const order = req.query.order || 'desc';//order ke according wrna desc mein
+
+    const listings = await Listing.find({ //get the lsiting
+      name: { $regex: searchTerm, $options: 'i' }, //regex functionaltiy for mongodb helps search for words ****option=i*** matlab lowercase and uppdercasse nhi dekhaega 
+
+      offer, //search for all order furnsiht tyep etc
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order }) //we wanna sort it on the  taken  order
+      .limit(limit) //and we limit and 
+      .skip(startIndex); //agar limit 9 and idex is 0 toh start 9 show krega warna 1 toh skip frist 9   
+
+    return res.status(200).json(listings);
   } catch (error) {
     next(error);
   }
