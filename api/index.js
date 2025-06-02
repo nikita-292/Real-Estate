c9b7be282e1426686e8dd07d5d16aa4aa84c74bb
nikita-292 <145786206+nikita-292@users.noreply.github.com>
2025-06-02 10:53:43 +0530

    ADD PREINSTALLATION

--- api/index.js
+++ api/index.js
@@ -4,7 +4,8 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
-import cookieParser from 'cookie-parser';  
+import cookieParser from 'cookie-parser'; 
+import path from 'path'; 
 dotenv.config();
 
 
@@ -15,6 +16,8 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err);
 });
 
+
+ const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json()); // middleware to parse json data from the request body
@@ -30,6 +33,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter); // use the user router for the api/user route
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter); // use the listing router for the api/listing route
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 //middleware error from ip  next call for the next middleware
 // Error-handling middleware
